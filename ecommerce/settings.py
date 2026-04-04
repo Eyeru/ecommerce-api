2f017406265a650b8897c41a94ac07610c6ae441
Eyeru <eyeru.yeshewa20@gmail.com>
2026-04-04 21:31:31 +0300

    update ecommerce/settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -112,7 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://ecommerce-api.onrender.com",
+    "https://ecommerce-api-0cow.onrender.com",
+]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
