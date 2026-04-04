f5a796eaf8bbad23f93cde52720e30a72f8f2adf
Eyeru <eyeru.yeshewa20@gmail.com>
2026-04-04 21:22:06 +0300

    update urls.py to use product_list as the name for the home page

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='home'),
+    path('', views.product_list, name='product_list'),
     path('api/products/', views.product_list, name='product_list'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
     path('add-to-cart/<int:product_id>/', views.add_to_cart, 
